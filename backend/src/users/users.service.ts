2d284e96799c17324a0ff1357f4f5477eac6b803
StiliyanKushev <stiliyankushev@gmail.com>
2024-02-23 12:50:47 +0200

    feat: make a basic crud users module/service

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,68 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const entity = this.usersRepository.create(createUserDto);
+      return await this.usersRepository.save(entity);
+    } catch (err) {
+      const pgUniqueViolationErrorCode = '23505';
+      if (err.code == pgUniqueViolationErrorCode) {
+        throw new ConflictException();
+      }
+      throw err;
+    }
+  }
+
+  async findOne(id: number) {
+    try {
+      return await this.usersRepository.findOneByOrFail({ id });
+    } catch {
+      throw new NotFoundException();
+    }
+  }
+
+  async findOneByEmail(email: string): Promise<User> {
+    try {
+      return await this.usersRepository.findOneByOrFail({ email });
+    } catch {
+      throw new NotFoundException();
+    }
+  }
+
+  async hasOne(id: number) {
+    return await this.usersRepository.existsBy({ id });
+  }
+
+  async hasOneByEmail(email: string) {
+    return await this.usersRepository.existsBy({ email });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const entity = await this.usersRepository.preload({ id, ...updateUserDto });
+
+    if (!entity) {
+      throw new NotFoundException('cannot update, user does not exist');
+    }
+
+    return this.usersRepository.update({ id }, entity);
+  }
+
+  remove(id: number) {
+    return this.usersRepository.delete({ id });
+  }
+}
