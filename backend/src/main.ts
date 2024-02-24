d86f9660765a49aef0296d789571a159704ea514
StiliyanKushev <stiliyankushev@gmail.com>
2024-02-24 02:01:41 +0200

    feat: add https letsencrypt certs to the nestjs app when prod

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,30 @@
+import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { CorsService } from 'cors/cors.service';
+import fs from 'fs';
 import { LoggingService } from 'logging/logging.service';
 import { SwaggerSetupService } from 'swagger-setup/swagger-setup.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const productionOptions: NestApplicationOptions =
+    process.env.ENVIRONMENT === 'production'
+      ? {
+          httpsOptions: {
+            key: fs.readFileSync(
+              '/etc/letsencrypt/live/api.shareprofitcalculator.site/privkey.pem',
+            ),
+            cert: fs.readFileSync(
+              '/etc/letsencrypt/live/api.shareprofitcalculator.site/fullchain.pem',
+            ),
+          },
+        }
+      : {};
+
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    ...productionOptions,
+  });
 
   // setup additional configurations required by core modules
   app.get(LoggingService).setup(app);
